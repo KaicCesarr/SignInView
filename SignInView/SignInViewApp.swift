5e25ed2fca247c174155e548ea4d2cbf1c604b20
Kaic Cesar <kaiccesar.dev@gmail.com>
2023-04-10 21:53:49 -0300

    Initial Commit

---
+++ SignInView/SignInViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewApp.swift
+//  SignInView
+//
+//  Created by Kaic Cesar on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SignInViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
